8501ef533d0fd4cab323c390afbb7e530dc2fc12
Nickisdope <53715843+Nickisdope@users.noreply.github.com>
2023-04-12 12:29:44 +0200

    add essential matrix computation, drawing match

--- main.py
+++ main.py
@@ -4,25 +4,35 @@ import matplotlib.pyplot as plt
 from calibrator import CameraCalibrator
 from feature_extractor import FeatureExtractor, FeatureMatcher
 from triangulator import Triangulator
+from pose_estimation import PoseEstimator
 
 def main():
     # Define calibration pattern size
-    pattern_size = (9, 6)
+    # pattern_size = (9, 6)
+    # pattern_size = (23, 31)
 
-    # Load calibration images
-    img_list = [cv2.imread('calibration_images/calibration{}.jpg'.format(i+1)) for i in range(10)]
+    # # Load calibration images
+    # img_list = [cv2.imread('calibration_images/camera_calib{}.jpg'.format(i+1)) for i in range(10)]
+    #
+    # # Calibrate camera
+    # calibrator = CameraCalibrator(pattern_size)
+    #
+    # # return intrinsic matrix and distortion coefficients
+    # mtx, dist = calibrator.calibrate(img_list)
 
-    # Calibrate camera
-    calibrator = CameraCalibrator(pattern_size)
-    mtx, dist = calibrator.calibrate(img_list)
+    # mannually input K
+    mtx = np.array([[518.86, 0., 285.58],
+                  [0., 519.47, 213.74],
+                  [0., 0., 1.]])
+    dist = np.zeros((5,))
 
     # Initialize feature extractor and feature matcher
     feature_extractor = FeatureExtractor(mtx, dist, method='sift')
-    feature_matcher = FeatureMatcher(matcher='bf')
+    feature_matcher = FeatureMatcher(matcher='flann')
 
     # Load images
-    img1 = cv2.imread('images/img1.jpg')
-    img2 = cv2.imread('images/img2.jpg')
+    img1 = cv2.imread('images/a1.png')
+    img2 = cv2.imread('images/a2.png')
 
     # Extract features and descriptors
     '''
@@ -38,11 +48,37 @@ def main():
     # Match features
     matches = feature_matcher.match(des1, des2)
 
+    # store all the good matches as per Lowe's ratio test.
+    # good = []
+    # for m, n in matches:
+    #     if m.distance < 0.7 * n.distance:
+    #         good.append(m)
+    # matches = good
+
+    draw_params = dict(matchColor=(0, 255, 0),  # draw matches in green color
+                       singlePointColor=None,
+                       flags=2)
+
+    img_siftmatch = cv2.drawMatches(img1, kp1, img2, kp2, matches, None, **draw_params)
+    cv2.imshow('SIFT_Matches', img_siftmatch)
+
+    # essential matrix
+    pose_estimator = PoseEstimator(mtx, dist)
+    R, t, mask = pose_estimator.estimate(kp1, kp2, matches)
+
+    matchesMask = mask.ravel().tolist()
+
+    draw_params = dict(matchColor=(0, 255, 0),  # draw matches in green color
+                       singlePointColor=None,
+                       matchesMask=matchesMask,  # draw only inliers
+                       flags=2)
+
+    img_inliermatch = cv2.drawMatches(img1, kp1, img2, kp2, matches, None, **draw_params)
+    cv2.imshow('Inlier_Matches', img_inliermatch)
+
     # Triangulate 3D points
-    R = np.eye(3)
-    t = np.zeros((3, 1))
     triangulator = Triangulator(mtx)
-    pts_3d = triangulator.triangulate(kp1, kp2, R, t, matches)
+    pts_3d = triangulator.triangulate(kp1, kp2, R, t, matches, matchesMask)
 
     # Visualize 3D points
     fig = plt.figure()
