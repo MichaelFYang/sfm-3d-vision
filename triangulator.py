3396d89fb7922496a9b5776fb056faa1abe13744
atuccillo <atuccillo@student.ethz.ch>
2023-04-13 17:59:19 +0200

    Pytorch version of triangulator.py

--- triangulator.py
+++ triangulator.py
@@ -27,19 +27,10 @@ class Triangulator:
         """
         pts1 = np.float32([kp1[m.queryIdx].pt for m in matches]).reshape(-1, 1, 2)
         pts2 = np.float32([kp2[m.trainIdx].pt for m in matches]).reshape(-1, 1, 2)
-
-        pts1 = pts1[np.asarray(matchesMask) == 1, :, :]
-        pts2 = pts2[np.asarray(matchesMask) == 1, :, :]
-
-        proj_mat1 = np.dot(self.mtx, np.hstack((np.eye(3), np.zeros((3, 1)))))
-        proj_mat2 = np.dot(self.mtx, np.hstack((R, t)))
-
+        proj_mat1 = np.hstack((np.eye(3), np.zeros((3, 1))))
+        proj_mat2 = np.hstack((R, t))
         proj_pts1 = cv2.undistortPoints(pts1, self.mtx, None)
         proj_pts2 = cv2.undistortPoints(pts2, self.mtx, None)
-
-        proj_pts1 = np.squeeze(proj_pts1).T
-        proj_pts2 = np.squeeze(proj_pts2).T
-
         pts_4d_hom = cv2.triangulatePoints(proj_mat1, proj_mat2, proj_pts1, proj_pts2)
         pts_3d_hom = pts_4d_hom / pts_4d_hom[3]
         pts_3d = pts_3d_hom[:3].T
