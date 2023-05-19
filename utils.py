773e8a18ce04d83eefb53f9a4928ac3adf29a3d1
Fan Yang <michael.yfan24@gmail.com>
2023-05-19 22:07:11 +0200

    working with LieTensor

--- utils.py
+++ utils.py
@@ -40,12 +40,11 @@ def visualize_LAF(img, LAF, img_idx = 0):
     plt.show()
     return
 
-def compute_reprojection_error(point3d, src_pts, dst_pts, R=None, T=None, K=None, P1=None, P2=None):
+def compute_reprojection_error(point3d, src_pts, dst_pts, R=None, T=None, K=None, P=None):
     """
     Input:
         point3d: array of triangulated 3D points in homo coordinate (N, 4)
-        P1: projection matrix of camera 1 (3, 4)
-        P2: projection matrix of camera 2 (3, 4)
+        P: projection matrix of camera 2 (3, 4)
         src_pts: original 2D image points of camera 1 (N, 2)
         dst_pts: original 2D image points of camera 2 (N, 2)
     """
@@ -60,12 +59,16 @@ def compute_reprojection_error(point3d, src_pts, dst_pts, R=None, T=None, K=None
     N = point3d.shape[0]
     point3d = torch.hstack((point3d, torch.ones((N, 1))))  # Convert to homogeneous coordinates
     
+    P1 = K @ torch.hstack((torch.eye(3), torch.zeros((3,1))))
     if R is not None and T is not None and K is not None:
-        P1 = K @ torch.hstack((torch.eye(3), torch.zeros((3,1))))
         P2 = K @ torch.hstack((R, T.reshape((3,1))))
+        reproj_2d_2 = point3d @ P2.T
+    else:
+        reproj_2d_2 = P @ point3d
+        reproj_2d_2 = (K @ reproj_2d_2[:,:-1].T).T
 
     reproj_2d_1 = point3d @ P1.T 
-    reproj_2d_2 = point3d @ P2.T
+    
 
     reproj_2d_1 = reproj_2d_1 / reproj_2d_1[:, -1].unsqueeze(1)
     reproj_2d_2 = reproj_2d_2 / reproj_2d_2[:, -1].unsqueeze(1)
