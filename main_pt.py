c55f5a1d5ebbe3e26e5e3a2c228cc4357b879753
Fan Yang <michael.yfan24@gmail.com>
2023-05-20 00:18:33 +0200

    not working - temp save

--- main_pt.py
+++ main_pt.py
@@ -1,8 +1,8 @@
 import cv2
 import torch
-import pypose as pp
 import numpy as np
 import matplotlib.pyplot as plt
+from pixel_adjuster_pt import PixelAdjuster
 from feature_extractor_pt import FeatureExtractor, FeatureMatcher
 from pose_estimiation_pt import PoseEstimator
 from bundle_adjuster_pt import BundleAdjuster
@@ -69,57 +69,20 @@ def main():
     # scores, matches = K.feature.match_fginn(des1, des2, kp1, kp2, mutual=True)
 
     # Now RANSAC
-    src_pts = kp1[0, matches[:,0], :, 2].float()
-    dst_pts = kp2[0, matches[:,1], :, 2].float()
+    src_pts = kp1[0, matches[:,0], :, 2].float().detach().requires_grad_(True)
+    dst_pts = kp2[0, matches[:,1], :, 2].float().detach().requires_grad_(True)
 
-    Fm, inliers = pose_estimator.compute_fundametal_matrix_kornia(src_pts, dst_pts)
-    src_pts = src_pts[inliers]
-    dst_pts = dst_pts[inliers]
-
-
-    Em = K.geometry.essential_from_fundamental(Fm, mtx_torch, mtx_torch)
-
-    R, T, point3d = pose_estimator.recover_pose(Em, src_pts, dst_pts, mtx_torch)
-    # R, T, point3d = K.geometry.epipolar.motion_from_essential_choose_solution(Em, mtx_torch, mtx_torch, src_pts, dst_pts, mask=None)
-
-    R_noise = get_noise_rotation(1.0)  # set your noise standard deviation
-    R_noisy = R.matmul(R_noise)
-
-    T_noise = torch.normal(mean=0.2, std=0.5, size=T.shape)  # set your noise standard deviation
-    T_noisy = T + T_noise
-
-    reproj_2d_1, reproj_2d_2, err = compute_reprojection_error(point3d, src_pts, dst_pts, R=R_noisy, T=T_noisy, K=mtx_torch)
-    
-    # print reprojection error
-    print('Average reprojection error: {}'.format(err))
-
-    # visualize the computation graph of one pass
-    # make_dot(err, params={'R': R, 'T': T, 'point3d': point3d}).render("err_torchviz", format="png")
-
-    # visualize projection
-    visualize_reprojection(img1, img2, src_pts, dst_pts, reproj_2d_1, reproj_2d_2)
-
-    # create leaf nodes that require grad for R, T, point3d
-    T_opt = torch.hstack((R_noisy, T_noisy.reshape((3,1)))).detach().requires_grad_(True)
-    T_pp_opt = pp.from_matrix(T_opt, ltype=pp.SE3_type).detach().requires_grad_(True)
-    point3d_opt = point3d.clone().detach().requires_grad_(True)
-    point3d_opt_lie = point3d_opt.clone().detach().requires_grad_(True)
-
-    # init Trainer
-    bundle_adjuster_Lie = BundleAdjuster(point3d_opt_lie, mtx_torch, src_pts.detach(), dst_pts.detach(), P=T_pp_opt, optimizer='adam')
-    bundle_adjuster_normal = BundleAdjuster(point3d_opt, mtx_torch, src_pts.detach(), dst_pts.detach(), P=T_opt, optimizer='adam')
+    pixel_opt = PixelAdjuster(src_pts=src_pts, dst_pts=dst_pts, K=mtx_torch)
 
     num_iters = 2000
 
     start_time = time.time()
 
-    err_Lie_all = []
     err_normal_all = []
 
     for i in range(num_iters):
         # train
-        reproj_2d_1_normal, reproj_2d_2_normal, err_normal = bundle_adjuster_normal.adjust_step()
-        reproj_2d_1_Lie, reproj_2d_2_Lie, err_Lie = bundle_adjuster_Lie.adjust_step()
+        reproj_2d_1_normal, reproj_2d_2_normal, err_normal = pixel_opt.adjust_step(pose_estimator)
 
         # check
         # make_dot(err, params={'R': R_opt, 'T': T_opt, 'point3d': point3d_opt}).render("err_torchviz", format="png")
@@ -127,21 +90,17 @@ def main():
         # print reprojection error
         print('==================== {}th Epoch ===================='.format(i))
         print('Normal average reprojection error: {}'.format(err_normal))
-        print('Lie average reprojection error: {}'.format(err_Lie))
 
-        err_Lie_all.append(err_Lie.item())
         err_normal_all.append(err_normal.item())
 
     # visualize projection
     visualize_reprojection(img1, img2, src_pts, dst_pts, reproj_2d_1_normal, reproj_2d_2_normal, key='normal')
-    visualize_reprojection(img1, img2, src_pts, dst_pts, reproj_2d_1_Lie, reproj_2d_2_Lie, key='Lie')
     
     end_time = time.time()
     execution_time = end_time - start_time
     print("Execution time: ", execution_time, " seconds")
 
     # Create the plot
-    plt.plot(err_Lie_all, label='Lie')
     plt.plot(err_normal_all, label='Normal')
 
     # Add labels and title
---
+++ pixel_adjuster_pt.py
@@ -0,0 +1,50 @@
+import torch
+import kornia as K
+from utils import compute_reprojection_error
+
+class PixelAdjuster:
+
+    def __init__(self, src_pts, dst_pts, K):
+        """
+        Constructor for BundleAdjuster class.
+
+        Parameters:
+        - R_opt: leaf node that requires grad for R
+        - T_opt: leaf node that requires grad for T
+        - point3d_opt: leaf node that requires grad for point3d
+        - K: intrinsic matrix
+        - src_pts: source points
+        - dst_pts: destination points
+        - optimizer: optimizer to use (adam or LBFGS)
+        """
+
+        self.src_pts = src_pts
+        self.dst_pts = dst_pts
+
+        self.optimizer = torch.optim.Adam([self.src_pts, self.dst_pts], lr=1e-3)
+            
+        self.loss = compute_reprojection_error
+        self.K = K
+
+    def adjust_step(self, pose_estimator):
+        """
+        Perform one adjustment step.
+
+        """
+        Fm, inliers = pose_estimator.compute_fundametal_matrix_kornia(self.src_pts, self.dst_pts)
+        self.src_pts = self.src_pts[inliers]
+        self.dst_pts = self.dst_pts[inliers]
+
+        Em = K.geometry.essential_from_fundamental(Fm, self.K, self.K)
+        R, T, point3d = pose_estimator.recover_pose(Em, self.src_pts, self.dst_pts, self.K)
+
+        # compute reprojection error
+        reproj_2d_1, reproj_2d_2, err = self.loss(point3d, self.src_pts, self.dst_pts, R=R, T=T, K=self.K) 
+        
+        # update R, T, point3d
+        
+        self.optimizer.zero_grad()
+        err.backward()
+        self.optimizer.step()
+
+        return reproj_2d_1, reproj_2d_2, err
